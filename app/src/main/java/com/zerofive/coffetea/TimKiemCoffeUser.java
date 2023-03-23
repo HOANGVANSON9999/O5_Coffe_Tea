a302337f576a51c8db5059b45e42fded0cd068dd
HOANGVANSON9999 <vson99990@gmail.com>
2023-03-23 09:51:15 +0700

    Timkiem

---
+++ app/src/main/java/com/zerofive/coffetea/TimKiemCoffeUser.java
@@ -0,0 +1,14 @@
+package com.zerofive.coffetea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimKiemCoffeUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tim_kiem_coffe_user);
+    }
+}
