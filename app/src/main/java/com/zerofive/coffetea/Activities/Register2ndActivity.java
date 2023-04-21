ab4c410307a97241aac6e68c01dda52bf36b83cf
son <vson99990@gmail.com>
2023-04-21 18:02:24 +0700

    reg2

---
+++ app/src/main/java/com/zerofive/coffetea/Activities/Register2ndActivity.java
@@ -0,0 +1,14 @@
+package com.zerofive.coffetea.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.zerofive.coffetea.R;
+public class Register2ndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2nd);
+    }
+}
