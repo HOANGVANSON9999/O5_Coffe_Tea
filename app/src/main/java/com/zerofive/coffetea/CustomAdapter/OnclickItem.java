231496b31cf3683e182c1efabe431f749589c241
son <vson99990@gmail.com>
2023-04-21 22:33:07 +0700

    d

--- app/src/main/java/com/zerofive/coffetea/CustomAdapter/OnclickItem.java
+++
@@ -1,9 +0,0 @@
-package com.zerofive.coffetea.CustomAdapter;
-
-import com.sinhvien.orderdrinkapp.DTO.ThanhToanDTO;
-
-public interface OnclickItem {
-    void OnClickBack(ThanhToanDTO thanhToanDTO);
-    void OnClickNext(ThanhToanDTO thanhToanDTO);
-    void OnLongClick(ThanhToanDTO thanhToanDTO);
-}
